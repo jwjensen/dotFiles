636371a3c4af07751b57f8ef1859f4729d0f928a
Jim Jensen <jensen@computer.org>
2016-04-07 22:40:24 -0400

    initial version emacs init.el

---
+++ emacs/init.el
@@ -0,0 +1,51 @@
+;; init.el --- Emacs configuration
+
+;; INSTALL PACKAGES
+;; --------------------------------------
+
+(require 'package)
+
+(add-to-list 'package-archives
+       '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar myPackages
+  '(better-defaults
+    elpy
+    flycheck
+    material-theme
+    py-autopep8
+    ))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+;; BASIC CUSTOMIZATION
+;; --------------------------------------
+
+(display-time)
+(setq display-time-and-date t)
+;; make next-line move by line, not displayed line
+(setq line-move-visual nil)
+
+(setq c-basic-offset 2)
+(global-set-key [home] 'beginning-of-buffer)
+(global-set-key [end] 'end-of-buffer)
+
+(global-set-key "\C-x\C-b" 'electric-buffer-list)
+
+(load-theme 'material t) ;; load material theme
+(elpy-enable)
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+
+;; init.el ends here
