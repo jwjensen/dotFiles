8eac9a85c7c26cd30d94ed03d7c5b65cf8a16781
Jim Jensen <jensen@p52s.jensen.dyndns.org>
2018-07-17 22:22:54 -0400

    change linum to darker grey

--- emacs/init.el
+++ emacs/init.el
@@ -6,6 +6,7 @@
  '(custom-safe-themes
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(global-linum-mode t)
  '(package-selected-packages
    (quote
     (fill-column-indicator smart-mode-line pdf-tools sql-indent pkg-info dired-du zenburn-theme plsql material-theme magit better-defaults)))
@@ -175,7 +176,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(linum ((t (:background "#263238" :foreground "dim gray")))))
 
 
 ;; https://github.com/kpurdon/.emacs.d
