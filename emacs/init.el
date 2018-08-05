69b1ed1bfae7a3ddc1aa6be36e3d07709916110b
Jim Jensen <jensen@computer.org>
2018-08-05 19:25:01 -0400

    add tabbar

--- emacs/init.el
+++ emacs/init.el
@@ -9,7 +9,7 @@
  '(global-linum-mode t)
  '(package-selected-packages
    (quote
-    (graphviz-dot-mode fill-column-indicator smart-mode-line pdf-tools sql-indent pkg-info dired-du zenburn-theme plsql material-theme magit better-defaults)))
+    (tabbar graphviz-dot-mode fill-column-indicator smart-mode-line pdf-tools sql-indent pkg-info dired-du zenburn-theme plsql material-theme magit better-defaults)))
  '(pyvenv-virtualenvwrapper-python "/usr/bin/python3")
  '(scroll-bar-mode (quote right)))
 
@@ -53,6 +53,7 @@
     web-mode
     csv-mode
     graphviz-dot-mode
+    tabbar
 ))
 
 (mapc #'(lambda (package)
