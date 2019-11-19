09daae86475a59b46dad171ab7dec6bf2997f8f7
Jim Jensen <jensen@computer.org>
2019-11-18 21:04:17 -0500

    resolve merge conflict

--- emacs/init.el
+++ emacs/init.el
@@ -10,7 +10,7 @@
  '(global-linum-mode t)
  '(package-selected-packages
    (quote
-    (ng2-mode neotree treemacs-icons-dired use-package treemacs tabbar graphviz-dot-mode fill-column-indicator smart-mode-line pdf-tools sql-indent pkg-info dired-du zenburn-theme plsql material-theme magit better-defaults)))
+    (virtualenvwrapper ng2-mode neotree treemacs-icons-dired use-package treemacs tabbar graphviz-dot-mode fill-column-indicator smart-mode-line pdf-tools sql-indent pkg-info dired-du zenburn-theme plsql material-theme magit better-defaults)))
  '(pyvenv-virtualenvwrapper-python "/usr/bin/python3")
  '(scroll-bar-mode (quote right))
  '(send-mail-function nil)
